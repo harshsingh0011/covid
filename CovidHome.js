821cb2525b716502a041d1ea4e45512b95dd9358
dev-node-at-404065758000 <mitesh.panchal@thinklayer.co.in>
2021-08-04 18:19:00 +0530

    first commit

---
+++ CovidHome.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+function Faq() {  /*onclick redirection to faq page*/
+    location.href = "Faq.html";
+
+};
+
+function News() { /*onclick redirection to News page*/
+    location.href = "News.html";
+
+};
+
+
+
